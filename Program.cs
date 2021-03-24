519afe167ce6f03be4146ff7dab2992d8eeb97e8
Alexey Goncharov <alex.goncharoff@gmail.com>
2021-03-24 23:33:23 +0300

    Fixed cars list not being printed to console

--- Program.cs
+++ Program.cs
@@ -32,7 +32,7 @@ namespace _7
 
             int amount2 = OurParking.FreeSpace();
             
-            //New part1
+            //New part1 solution
             Car car2 = new Car();
             
             Console.WriteLine($"We have now only {amount2} parking lots\n");
